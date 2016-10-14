1918223567feed42d309792a3c001239c5f47f5e
mdrubinstein <mdrubinstein@gmail.com>
2016-10-13 20:59:41 -0400

    MinorEdits

--- 02_RPgrms/00_data-processing.R
+++ 02_RPgrms/00_data-processing.R
@@ -1,7 +1,7 @@
 # Program: 00_data-processing-cm.R
 # Purpose: Create crosswalk from ICD-9-CM to ICD-10-CM
 # Author:  Max Rubinstein
-# Date last modified: October 9, 2016
+# Date last modified: October 13, 2016
 
 # Load libraries ----------------------------------------------------------------
 library('purrr')
@@ -54,31 +54,30 @@ process_data <- . %>%
   order_cols(col_order) %>%
   mutate(duplicate_mapping = df_sub_dup(., c('icd_9', 'icd_10')))
 
+# Execute all funs --------------------------------------------------------------
+all_funs <- . %>%
+  read_data() %>%
+  stack_xwalk() %>%
+  join_description() %>%
+  process_data()
+
 # Execute funs for CM codes -----------------------------------------------------
+col_order <- '01_AnalyticFiles/varnames.yaml' %>% yaml_process()
 i9to10    <- '00_RawData/2016_I9gem.txt'
 i10to9    <- '00_RawData/2016_I10gem.txt'
 i9names   <- '00_RawData/CMS32_DESC_LONG_SHORT_DX.xlsx'
 i10names  <- '00_RawData/icd10cm_codes_2016.txt'
-col_order <- '01_AnalyticFiles/varnames.yaml' %>% yaml_process()
 
-processed_data_cm <- i9to10 %>%
-                     read_data() %>%
-                     stack_xwalk() %>%
-                     join_description() %>%
-                     process_data()
+processed_data_cm  <- i9to10 %>% all_funs()
 
 saveRDS(processed_data_cm, '01_AnalyticFiles/cm_mapping_2016.RDS')
 
 # Execute funs for PCS codes -----------------------------------------------------
-i9to10_pcs <- '00_RawData/gem_i9pcs.txt'
-i10to9_pcs <- '00_RawData/gem_pcsi9.txt'
+i9to10     <- '00_RawData/gem_i9pcs.txt'
+i10to9     <- '00_RawData/gem_pcsi9.txt'
 i9names    <- '00_RawData/CMS32_DESC_LONG_SHORT_SG.xlsx'
 i10names   <- '00_RawData/icd10pcs_codes_2016.txt'
 
-processed_data_pcs <- i9to10_pcs %>%
-                      read_data() %>%
-                      stack_xwalk() %>%
-                      join_description() %>%
-                      process_data()
+processed_data_pcs <- i9to10 %>% all_funs()
 
 saveRDS(processed_data_pcs, '01_AnalyticFiles/pcs_mapping_2016.RDS')
